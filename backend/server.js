1a411e72e133397c54f746455c90f6c4085da0a5
Amal221200 <amalmurikkoli2000@gmail.com>
2022-04-03 11:19:53 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 dbConnect()
 
-const port = process.env.PORT ? process.env.PORT : 5000
+const port = process.env.PORT || 5000
 
 
 app.use(express.json())
@@ -18,14 +18,19 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    )
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
+
 app.listen(port, () => {
     console.log(`Server listening at port ${port}`)
 })
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../frontend/build")))
-
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html")))
-} else {
-    app.get("/", (req, res) => res.send("Please set to Production"))
-}
